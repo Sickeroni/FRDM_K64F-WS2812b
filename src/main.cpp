ddc1b61c03bc04f8d8fe5e3f76660e9529470a69
Paul Targosz <paultargosz86@googlemail.com>
2018-01-06 19:01:27 +0100

    init version that compiles

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <MK64F12.h>
+#include "fsl_device_registers.h"
+
+extern "C" {
+
+
+int main(void); // needed here to be callable in main.c (otherwise name mangeling mismatch)
+}
+
+
+
+
+
+int main()
+{
+    /*
+    SYSMPU_Type *base = SYSMPU;
+    BOARD_InitPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+    // Disable SYSMPU.
+    base->CESR &= ~SYSMPU_CESR_VLD_MASK; */
+    while (1)
+    {
+
+    }
+}
